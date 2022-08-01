b76068e3ca9ad761c8b938a8afaea7d8917bfd16
longvb <vuongbaolong48@gmail.com>
2022-08-01 17:28:46 +0000

    update images

--- components/RegisterCard.tsx
+++ components/RegisterCard.tsx
@@ -5,126 +5,146 @@ import SessionContext from "../context/session-context";
 import { useLogoutHandler } from "../pkg";
 
 interface Props {
-  id: string | string[] | undefined;
+    id: string | string[] | undefined;
 }
 
 function RegisterCard(props: Props) {
-  const { session } = useContext(SessionContext);
-  const router = useRouter();
-  const { id } = props;
+    const { session } = useContext(SessionContext);
+    const router = useRouter();
+    const { id } = props;
 
-  const logout = useLogoutHandler();
-  const [active, setActive] = useState<number>(1);
-  const onRegister = async () => {
-    if (session) {
-      await logout();
-    }
-    router.push(`/register?card_id=${id}`);
-  };
+    const logout = useLogoutHandler();
+    const [active, setActive] = useState<number>(1);
+    const onRegister = async () => {
+        if (session) {
+            await logout();
+        }
+        router.push(`/register?card_id=${id}`);
+    };
 
-  return (
-    <section className="section active">
-      <div className="homecolor-box" />
-      <div className="common_bg animate__animated animate__zoomIn">
-        <div className="container m-auto">
-          <div className="row align-items-center">
-            <div className="col-12">
-              <div className="home-content text-center">
-                <div className="onboarding-container">
-                  <div
-                    className="steps"
-                    style={{
-                      transform: `translateX(${-(100 / 3) * (active - 1)}%)`,
-                    }}
-                  >
-                    <div className="step">
-                      <div className="image-container">
-                        <img
-                          className="step-image"
-                          src="/images/onboarding/image1.png"
-                          alt=""
-                        />
-                      </div>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Labore, reiciendis doloremque pariatur aperiam autem
-                      laboriosam tempora, quam sint consequatur veritatis
-                      incidunt! Ipsa et hic sit aspernatur temporibus architecto
-                      fuga corporis.
+    return (
+        <section className="section active">
+            <div className="homecolor-box" />
+            <div className="common_bg animate__animated animate__zoomIn">
+                <div className="container m-auto">
+                    <div className="row align-items-center">
+                        <div className="col-12">
+                            <div className="home-content text-center">
+                                <div className="onboarding-container">
+                                    <div
+                                        className="steps"
+                                        style={{
+                                            transform: `translateX(${
+                                                -(100 / 3) * (active - 1)
+                                            }%)`,
+                                        }}
+                                    >
+                                        <div className="step">
+                                            <div className="image-container">
+                                                <img
+                                                    className="step-image"
+                                                    src="/images/onboarding/tech.png"
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="step-description">
+                                                <p>
+                                                    Chào mừng đến với hệ sinh
+                                                    thái smartcard.
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="step">
+                                            <div className="image-container">
+                                                <img
+                                                    className="step-image"
+                                                    src="/images/onboarding/image1.png"
+                                                    alt=""
+                                                />
+                                            </div>
+
+                                            <div className="step-description">
+                                                <p>
+                                                    Smart card là thẻ thông minh
+                                                    cho cá nhân và doanh nghiệp.
+                                                    Với smartcard, bạn có thể
+                                                    tiện lợi chia sẻ thông tin
+                                                    cho khách hàng cá nhân và
+                                                    tích hợp quản trị doanh
+                                                    nghiệp
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div className="step">
+                                            <div className="image-container">
+                                                <img
+                                                    className="step-image"
+                                                    src="/images/onboarding/social.png"
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="step-description">
+                                                <p>
+                                                    Đăng ký ngay và bắt đầu trải nghiệm
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="btn-container ">
+                                        <div
+                                            className={`skip-btn ${
+                                                active === 3 ? "btn-hiden" : ""
+                                            }`}
+                                            onClick={() => {
+                                                setActive(3);
+                                            }}
+                                        >
+                                            Bỏ qua
+                                        </div>
+                                        <div className="dots">
+                                            <div
+                                                className={`dot ${
+                                                    active === 1 ? "active" : ""
+                                                }`}
+                                            ></div>
+                                            <div
+                                                className={`dot ${
+                                                    active === 2 ? "active" : ""
+                                                }`}
+                                            ></div>
+                                            <div
+                                                className={`dot ${
+                                                    active === 3 ? "active" : ""
+                                                }`}
+                                            ></div>
+                                        </div>
+                                        <div
+                                            className={`next-btn ${
+                                                active === 3 ? "btn-hiden" : ""
+                                            }`}
+                                            onClick={() => {
+                                                setActive((prep) => prep + 1);
+                                            }}
+                                        >
+                                            Tiếp tục
+                                        </div>
+                                    </div>
+                                    <button
+                                        className={`btn btn-link ${
+                                            active === 3 ? "" : "btn-hiden"
+                                        }`}
+                                        onClick={onRegister}
+                                    >
+                                        Đăng ký ngay
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="step">
-                      <div className="image-container">
-                        <img
-                          className="step-image"
-                          src="/images/onboarding/image2.png"
-                          alt=""
-                        />
-                      </div>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Labore, reiciendis doloremque pariatur aperiam autem
-                      laboriosam tempora, quam sint consequatur veritatis
-                      incidunt! Ipsa et hic sit aspernatur temporibus architecto
-                      fuga corporis.
-                    </div>
-                    <div className="step">
-                      <div className="image-container">
-                        <img
-                          className="step-image"
-                          src="/images/onboarding/image3.png"
-                          alt=""
-                        />
-                      </div>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Labore, reiciendis doloremque pariatur aperiam autem
-                      laboriosam tempora, quam sint consequatur veritatis
-                      incidunt! Ipsa et hic sit aspernatur temporibus architecto
-                      fuga corporis.
-                    </div>
-                  </div>
-                  <div className="btn-container ">
-                    <div
-                      className={`skip-btn ${active === 3 ? "btn-hiden" : ""}`}
-                      onClick={() => {
-                        setActive(3);
-                      }}
-                    >
-                      Skip
-                    </div>
-                    <div className="dots">
-                      <div
-                        className={`dot ${active === 1 ? "active" : ""}`}
-                      ></div>
-                      <div
-                        className={`dot ${active === 2 ? "active" : ""}`}
-                      ></div>
-                      <div
-                        className={`dot ${active === 3 ? "active" : ""}`}
-                      ></div>
-                    </div>
-                    <div
-                      className={`next-btn ${active === 3 ? "btn-hiden" : ""}`}
-                      onClick={() => {
-                        setActive((prep) => prep + 1);
-                      }}
-                    >
-                      Next
-                    </div>
-                  </div>
-                  <button
-                    className={`btn btn-link ${
-                      active === 3 ? "" : "btn-hiden"
-                    }`}
-                    onClick={onRegister}
-                  >
-                    Đăng ký ngay
-                  </button>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 export default RegisterCard;
