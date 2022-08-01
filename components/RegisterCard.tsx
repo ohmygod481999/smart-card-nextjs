8c206f63fb5f201d3ccab2552d3db11923b10aca
Vanfulong <vanfulong@gmail.com>
2022-08-01 19:57:53 +0700

    add onboarding

--- components/RegisterCard.tsx
+++ components/RegisterCard.tsx
@@ -1,72 +1,130 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import SessionContext from "../context/session-context";
 import { useLogoutHandler } from "../pkg";
 
 interface Props {
-    id: string | string[] | undefined;
+  id: string | string[] | undefined;
 }
 
 function RegisterCard(props: Props) {
-    const { session } = useContext(SessionContext);
-    const router = useRouter();
-    const { id } = props;
+  const { session } = useContext(SessionContext);
+  const router = useRouter();
+  const { id } = props;
 
-    const logout = useLogoutHandler();
+  const logout = useLogoutHandler();
+  const [active, setActive] = useState<number>(1);
+  const onRegister = async () => {
+    if (session) {
+      await logout();
+    }
+    router.push(`/register?card_id=${id}`);
+  };
 
-    const onRegister = async () => {
-        if (session) {
-            await logout();
-        }
-        router.push(`/register?card_id=${id}`);
-    };
-
-    return (
-        <section className="section active">
-            <div className="homecolor-box" />
-            <div className="common_bg animate__animated animate__zoomIn">
-                <div className="container m-auto">
-                    <div className="row align-items-center">
-                        <div className="col-12">
-                            <div className="home-content text-center">
-                                <p className="common-desctiption animate__animated animate__fadeInDown animate__delay-1s">
-                                    Thẻ chưa kích hoạt
-                                </p>
-                                <p>
-                                    <button
-                                        className="btn btn-link"
-                                        onClick={onRegister}
-                                    >
-                                        Đăng ký
-                                    </button>
-                                    ngay
-                                    {/* <Link href={`/user/${id}/registration`}>Đăng ký</Link>  */}
-                                </p>
-                                {/* <div className="home-btn">
-                                    <a
-                                        href="#contact"
-                                        data-section-index={1}
-                                        className="clickbtn hire-me animate__animated animate__fadeInTopLeft animate__delay-3s "
-                                    >
-                                        Hire me
-                                    </a>
-                                    <a
-                                        href="#about"
-                                        data-section-index={1}
-                                        className="clickbtn about-us animate__animated animate__fadeInTopRight animate__delay-3s"
-                                    >
-                                        about me{" "}
-                                    </a>
-                                </div> */}
-                            </div>
-                        </div>
-                        {/* Profile-Information End */}
+  return (
+    <section className="section active">
+      <div className="homecolor-box" />
+      <div className="common_bg animate__animated animate__zoomIn">
+        <div className="container m-auto">
+          <div className="row align-items-center">
+            <div className="col-12">
+              <div className="home-content text-center">
+                <div className="onboarding-container">
+                  <div
+                    className="steps"
+                    style={{
+                      transform: `translateX(${-(100 / 3) * (active - 1)}%)`,
+                    }}
+                  >
+                    <div className="step">
+                      <div className="image-container">
+                        <img
+                          className="step-image"
+                          src="/images/onboarding/image1.png"
+                          alt=""
+                        />
+                      </div>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Labore, reiciendis doloremque pariatur aperiam autem
+                      laboriosam tempora, quam sint consequatur veritatis
+                      incidunt! Ipsa et hic sit aspernatur temporibus architecto
+                      fuga corporis.
                     </div>
+                    <div className="step">
+                      <div className="image-container">
+                        <img
+                          className="step-image"
+                          src="/images/onboarding/image2.png"
+                          alt=""
+                        />
+                      </div>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Labore, reiciendis doloremque pariatur aperiam autem
+                      laboriosam tempora, quam sint consequatur veritatis
+                      incidunt! Ipsa et hic sit aspernatur temporibus architecto
+                      fuga corporis.
+                    </div>
+                    <div className="step">
+                      <div className="image-container">
+                        <img
+                          className="step-image"
+                          src="/images/onboarding/image3.png"
+                          alt=""
+                        />
+                      </div>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Labore, reiciendis doloremque pariatur aperiam autem
+                      laboriosam tempora, quam sint consequatur veritatis
+                      incidunt! Ipsa et hic sit aspernatur temporibus architecto
+                      fuga corporis.
+                    </div>
+                  </div>
+                  <div className="btn-container ">
+                    <div
+                      className={`skip-btn ${active === 3 ? "btn-hiden" : ""}`}
+                      onClick={() => {
+                        setActive(3);
+                      }}
+                    >
+                      Skip
+                    </div>
+                    <div className="dots">
+                      <div
+                        className={`dot ${active === 1 ? "active" : ""}`}
+                      ></div>
+                      <div
+                        className={`dot ${active === 2 ? "active" : ""}`}
+                      ></div>
+                      <div
+                        className={`dot ${active === 3 ? "active" : ""}`}
+                      ></div>
+                    </div>
+                    <div
+                      className={`next-btn ${active === 3 ? "btn-hiden" : ""}`}
+                      onClick={() => {
+                        setActive((prep) => prep + 1);
+                      }}
+                    >
+                      Next
+                    </div>
+                  </div>
+                  <button
+                    className={`btn btn-link ${
+                      active === 3 ? "" : "btn-hiden"
+                    }`}
+                    onClick={onRegister}
+                  >
+                    Đăng ký ngay
+                  </button>
                 </div>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default RegisterCard;
