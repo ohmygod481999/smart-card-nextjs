6d6bac9fa0345dda14210a8aeaf216d4b801d4fa
longvb <vuongbaolong48@gmail.com>
2022-10-06 13:37:40 +0000

    ID => email while login

--- pkg/ui/NodeInputDefault.tsx
+++ pkg/ui/NodeInputDefault.tsx
@@ -24,7 +24,7 @@ export function NodeInputDefault<T>(props: NodeInputProps) {
   // Render a generic text input field.
   return (
     <TextInput
-      title={node.meta.label?.text}
+      title={node.meta.label?.text === "ID" ? "Email" : node.meta.label?.text}
       onClick={onClick}
       onChange={(e) => {
         setValue(e.target.value)
